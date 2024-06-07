f392fead0c6a4623e4adab83a35adb9a92583be8
DOOMNOTRON <edgarcruztacoma@gmail.com>
2024-06-07 02:14:19 -0700

    installed icon package and added work history

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 I will build a portfolio website using react, vite, and tailwindcss.
 
-## Requierments
+## Requirements
 1. [VS Code](https://code.visualstudio.com/)
 2. [Node JS](https://nodejs.org/en)
 3. [GitHub Account](https://www.google.com/aclk?sa=l&ai=DChcSEwjQwr2s-siGAxVoFK0GHTr5AnsYABAAGgJwdg&ase=2&gclid=CjwKCAjw34qzBhBmEiwAOUQcF8807LeWK9XjEiSX_Ikiy-t9Mwyusn2JzeXN5P7oKgpEQFQJq3BF8hoCwSEQAvD_BwE&ei=8rRiZpueMou-0PEPkfmagAI&sig=AOD64_2-8U4GsKOs7gAmbMsa3Yqu9OOh4w&q&sqi=2&nis=4&adurl&ved=2ahUKEwib37is-siGAxULHzQIHZG8BiAQ0Qx6BAgIEAE)
---
+++ src/constants/index.js
@@ -0,0 +1,71 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer who loves to create. I have worked with a variety of technologies, including React, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began when I was layed off and attempted to learn to code. Outside of coding, I enjoy spending time with my bunnies, exploring new technologies, and learning about entering a game jam.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Tool Maker",
+    company: "Boeing",
+    description: `I receive the plans, from engineering, for a new tool to be created to assist in the production of commercial airplane. We take the blueprints and build the tool based on the specifications. We also provide maintenance to and reword pre-existing tools.`,
+    technologies: ["REDARS", "CITRIX"],
+  },
+  {
+    year: "2018 - 2020",
+    role: "Hand Finisher",
+    company: "Boeing",
+    description: `I received aluminum airplane wings from the mills to smooth out the sharp edges and form them to the specifications on the blueprints.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2005 - 2014",
+    role: "Front office coordinator",
+    company: "Jasmin Terrace at El Molino",
+    description: `I was the first point of contact for the families that came to visit their loved ones. I handled the security and phone systems.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  
+
+];
+
+export const PROJECTS = [
+  {
+    title: "PLACE_HOLDER",
+    image: project1,
+    description:
+      "PLACE_HOLDER",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "PLACE_HOLDER",
+    image: project2,
+    description:
+      "PLACE_HOLDER",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "PLACE_HOLDER",
+    image: project3,
+    description:
+      "PLACE_HOLDER",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "PLACE_HOLDER",
+    image: project4,
+    description:
+      "PLACE_HOLDER",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "PLACE_HOLDER",
+  phoneNo: "PLACE_HOLDER",
+  email: "edgarcruztacoma@gmail.com",
+};
