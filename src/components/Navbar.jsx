84202d95f7ebccfa31d76c690261245290ee62f6
DOOMNOTRON <edgarcruztacoma@gmail.com>
2024-06-07 04:01:39 -0700

    Made Icons visible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,21 @@ import logo from "../assets/EC_Initials.svg";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 
 const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img src={logo} alt="logo"
-                    width="4%"
+                <img className=" mx-1 w-20" src={logo} alt="logo"
+                    
                 />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
                 <FaLinkedin />
                 <FaGithub />
+                <FaYoutube />
                 <FaInstagram />
             </div>
         </nav>
